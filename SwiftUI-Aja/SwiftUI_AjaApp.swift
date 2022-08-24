737c2adcd873b2786a4fdb4c7d5254480221d972
Ahlul Esasjana <cakahlul@gmail.com>
2022-08-24 19:35:17 +0700

    Initial Commit

---
+++ SwiftUI-Aja/SwiftUI_AjaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AjaApp.swift
+//  SwiftUI-Aja
+//
+//  Created by Ahlul Esasjana on 24/08/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AjaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
